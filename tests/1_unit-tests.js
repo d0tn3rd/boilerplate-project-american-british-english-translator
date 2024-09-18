f1f948de8441ac68817f95b2d9bf363086f73650
d0tn3rd <d0tn3rd@mailbox.org>
2024-09-18 17:00:43 +0530

    update unit tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -8,7 +8,7 @@ suite("Unit Tests", () => {
     test("Translate Mangoes are my favorite fruit. to British English", function () {
       assert.equal(
         Translator.toBritishEnglish("Mangoes are my favorite fruit.").replace(
-          /<span\s[='a-z]*>|<\/span>/g,
+          /<span\s[="a-z]*>|<\/span>/g,
           "",
         ),
         "Mangoes are my favourite fruit.",
@@ -18,7 +18,7 @@ suite("Unit Tests", () => {
     test("Translate I ate yogurt for breakfast. to British English", function () {
       assert.equal(
         Translator.toBritishEnglish("I ate yogurt for breakfast.").replace(
-          /<span\s[='a-z]*>|<\/span>/g,
+          /<span\s[="a-z]*>|<\/span>/g,
           "",
         ),
         "I ate yoghurt for breakfast.",
@@ -29,7 +29,7 @@ suite("Unit Tests", () => {
       assert.equal(
         Translator.toBritishEnglish(
           "We had a party at my friend's condo.",
-        ).replace(/<span\s[='a-z]*>|<\/span>/g, ""),
+        ).replace(/<span\s[="a-z]*>|<\/span>/g, ""),
         "We had a party at my friend's flat.",
       );
     });
@@ -38,14 +38,14 @@ suite("Unit Tests", () => {
       assert.equal(
         Translator.toBritishEnglish(
           "Can you toss this in the trashcan for me?",
-        ).replace(/<span\s[='a-z]*>|<\/span>/g, ""),
+        ).replace(/<span\s[="a-z]*>|<\/span>/g, ""),
         "Can you toss this in the bin for me?",
       );
     });
     test("Translate The parking lot was full. to British English", function () {
       assert.equal(
         Translator.toBritishEnglish("The parking lot was full.").replace(
-          /<span\s[='a-z]*>|<\/span>/g,
+          /<span\s[="a-z]*>|<\/span>/g,
           "",
         ),
         "The car park was full.",
@@ -57,7 +57,7 @@ suite("Unit Tests", () => {
       assert.equal(
         Translator.toBritishEnglish(
           "Like a high tech Rube Goldberg machine.",
-        ).replace(/<span\s[='a-z]*>|<\/span>/g, ""),
+        ).replace(/<span\s[="a-z]*>|<\/span>/g, ""),
         "Like a high tech Heath Robinson device.",
       );
     });
@@ -66,7 +66,7 @@ suite("Unit Tests", () => {
       assert.equal(
         Translator.toBritishEnglish(
           "To play hooky means to skip class or work.",
-        ).replace(/<span\s[='a-z]*>|<\/span>/g, ""),
+        ).replace(/<span\s[="a-z]*>|<\/span>/g, ""),
         "To bunk off means to skip class or work.",
       );
     });
@@ -76,7 +76,7 @@ suite("Unit Tests", () => {
       assert.equal(
         Translator.toBritishEnglish(
           "No Mr. Bond, I expect you to die.",
-        ).replace(/<span\s[='a-z]*>|<\/span>/g, ""),
+        ).replace(/<span\s[="a-z]*>|<\/span>/g, ""),
         "No Mr Bond, I expect you to die.",
       );
     });
@@ -85,7 +85,7 @@ suite("Unit Tests", () => {
     test("Translate Dr. Grosh will see you now. to British English", function () {
       assert.equal(
         Translator.toBritishEnglish("Dr. Grosh will see you now.").replace(
-          /<span\s[='a-z]*>|<\/span>/g,
+          /<span\s[="a-z]*>|<\/span>/g,
           "",
         ),
         "Dr Grosh will see you now.",
@@ -96,7 +96,7 @@ suite("Unit Tests", () => {
     test("Translate Lunch is at 12:15 today. to British English", function () {
       assert.equal(
         Translator.toBritishEnglish("Lunch is at 12:15 today.").replace(
-          /<span\s[='a-z]*>|<\/span>/g,
+          /<span\s[="a-z]*>|<\/span>/g,
           "",
         ),
         "Lunch is at 12.15 today.",
@@ -107,7 +107,7 @@ suite("Unit Tests", () => {
       assert.equal(
         Translator.toAmericanEnglish(
           "We watched the footie match for a while.",
-        ).replace(/<span\s[='a-z]*>|<\/span>/g, ""),
+        ).replace(/<span\s[="a-z]*>|<\/span>/g, ""),
         "We watched the soccer match for a while.",
       );
     });
@@ -116,7 +116,7 @@ suite("Unit Tests", () => {
       assert.equal(
         Translator.toAmericanEnglish(
           "Paracetamol takes up to an hour to work.",
-        ).replace(/<span\s[='a-z]*>|<\/span>/g, ""),
+        ).replace(/<span\s[="a-z]*>|<\/span>/g, ""),
         "Tylenol takes up to an hour to work.",
       );
     });
@@ -124,7 +124,7 @@ suite("Unit Tests", () => {
     test("Translate First, caramelise the onions. to American English", function () {
       assert.equal(
         Translator.toAmericanEnglish("First, caramelise the onions.").replace(
-          /<span\s[='a-z]*>|<\/span>/g,
+          /<span\s[="a-z]*>|<\/span>/g,
           "",
         ),
         "First, caramelize the onions.",
@@ -135,7 +135,7 @@ suite("Unit Tests", () => {
       assert.equal(
         Translator.toAmericanEnglish(
           "I spent the bank holiday at the funfair.",
-        ).replace(/<span\s[='a-z]*>|<\/span>/g, ""),
+        ).replace(/<span\s[="a-z]*>|<\/span>/g, ""),
         "I spent the public holiday at the carnival.",
       );
     });
@@ -144,7 +144,7 @@ suite("Unit Tests", () => {
       assert.equal(
         Translator.toAmericanEnglish(
           "I had a bicky then went to the chippy.",
-        ).replace(/<span\s[='a-z]*>|<\/span>/g, ""),
+        ).replace(/<span\s[="a-z]*>|<\/span>/g, ""),
         "I had a cookie then went to the fish-and-chip shop.",
       );
     });
@@ -152,7 +152,7 @@ suite("Unit Tests", () => {
       assert.equal(
         Translator.toAmericanEnglish(
           "I've just got bits and bobs in my bum bag.",
-        ).replace(/<span\s[='a-z]*>|<\/span>/g, ""),
+        ).replace(/<span\s[="a-z]*>|<\/span>/g, ""),
         "I've just got odds and ends in my fanny pack.",
       );
     });
@@ -160,7 +160,7 @@ suite("Unit Tests", () => {
       assert.equal(
         Translator.toAmericanEnglish(
           "The car boot sale at Boxted Airfield was called off.",
-        ).replace(/<span\s[='a-z]*>|<\/span>/g, ""),
+        ).replace(/<span\s[="a-z]*>|<\/span>/g, ""),
         "The swap meet at Boxted Airfield was called off.",
       );
     });
@@ -168,7 +168,7 @@ suite("Unit Tests", () => {
     test("Translate Have you met Mrs Kalyani? to American English", function () {
       assert.equal(
         Translator.toAmericanEnglish("Have you met Mrs Kalyani?").replace(
-          /<span\s[='a-z]*>|<\/span>/g,
+          /<span\s[="a-z]*>|<\/span>/g,
           "",
         ),
         "Have you met Mrs. Kalyani?",
@@ -180,7 +180,7 @@ suite("Unit Tests", () => {
       assert.equal(
         Translator.toAmericanEnglish(
           "Prof Joyner of King's College, London.",
-        ).replace(/<span\s[='a-z]*>|<\/span>/g, ""),
+        ).replace(/<span\s[="a-z]*>|<\/span>/g, ""),
         "Prof. Joyner of King's College, London.",
       );
     });
@@ -189,7 +189,7 @@ suite("Unit Tests", () => {
       assert.equal(
         Translator.toAmericanEnglish(
           "Tea time is usually around 4 or 4.30.",
-        ).replace(/<span\s[='a-z]*>|<\/span>/g, ""),
+        ).replace(/<span\s[="a-z]*>|<\/span>/g, ""),
         "Tea time is usually around 4 or 4:30.",
       );
     });
@@ -199,14 +199,14 @@ suite("Unit Tests", () => {
     test("Highlight translation in Mangoes are my favorite fruit.", () => {
       assert.equal(
         Translator.toBritishEnglish("Mangoes are my favorite fruit."),
-        "Mangoes are my <span class='highlight'>favourite</span> fruit.",
+        'Mangoes are my <span class="highlight">favourite</span> fruit.',
       );
     });
 
     test("Highlight translation in I ate yogurt for breakfast.", () => {
       assert.equal(
         Translator.toBritishEnglish("I ate yogurt for breakfast."),
-        "I ate <span class='highlight'>yoghurt</span> for breakfast.",
+        'I ate <span class="highlight">yoghurt</span> for breakfast.',
       );
     });
 
@@ -215,7 +215,7 @@ suite("Unit Tests", () => {
         Translator.toAmericanEnglish(
           "We watched the footie match for a while.",
         ),
-        "We watched the <span class='highlight'>soccer</span> match for a while.",
+        'We watched the <span class="highlight">soccer</span> match for a while.',
       );
     });
 
@@ -224,7 +224,7 @@ suite("Unit Tests", () => {
         Translator.toAmericanEnglish(
           "Paracetamol takes up to an hour to work.",
         ),
-        "<span class='highlight'>Tylenol</span> takes up to an hour to work.",
+        '<span class="highlight">Tylenol</span> takes up to an hour to work.',
       );
     });
   });
